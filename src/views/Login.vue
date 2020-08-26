bb5406f669726aeb573455a45c9a775b61adb48b
1042825973@qq.com <1042825973@qq.com>
2020-08-26 20:34:16 +0800

    项目目录配置路由

---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>我是login组件</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/views/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>我是regiter组件</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style></style>
