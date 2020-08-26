e33c7b82d427c2669fc64192a42a15bcefbc6ee2
1042825973@qq.com <1042825973@qq.com>
2020-08-26 22:56:29 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style/base.less'
+import './style/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// import Vue from 'vue'
+// import { Button } from 'vant'
+Vue.use(Vant)
+// Vue.use(Button)
 
 Vue.config.productionTip = false
 
