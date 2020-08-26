293f03c5c60f98dc07e942f121e6f78bd460e492
1042825973@qq.com <1042825973@qq.com>
2020-08-26 21:45:59 +0800

    错误完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Login from '../views/Login'
+import Register from '../views/Register'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/register', component: Register }
+]
 
 const router = new VueRouter({
   routes
